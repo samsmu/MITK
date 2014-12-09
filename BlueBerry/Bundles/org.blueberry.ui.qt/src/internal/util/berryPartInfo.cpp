b229d429062dd0f75e1a4b0b6cee2ac62b298621
is.moshkov <il.moshkov@gmail.com>
2014-12-09 19:54:11 +0400

    add temporary support plugin name translate AUT-47

--- BlueBerry/Bundles/org.blueberry.ui.qt/src/internal/util/berryPartInfo.cpp
+++ BlueBerry/Bundles/org.blueberry.ui.qt/src/internal/util/berryPartInfo.cpp
@@ -19,10 +19,32 @@ See LICENSE.txt or http://www.mitk.org for details.
 namespace berry
 {
 
+// TODO ism temporary translate
+// it hart to find point, when plugin name read from file
+
+const QString PartInfo::plugingTranslateNames[] = { 
+  QObject::tr("BasicImageProcessing"),   
+  QObject::tr("Display"),
+  QObject::tr("Data Manager"),
+  QObject::tr("Image Navigator"),
+  QObject::tr("LiverSegmentation"),
+  QObject::tr("Measurement"),
+  QObject::tr("PointSet Interaction"),
+  QObject::tr("RegionGrowing Segmentation"),
+  QObject::tr("Segmentation"),
+  QObject::tr("Segmentation Utilities"),
+  QObject::tr("Statistics"),
+  QObject::tr("Vascular Structure Segmentation"),
+  QObject::tr("Volume Visualization")
+};
+
+
 PartInfo::PartInfo() :
   image(0)
 {
-
+  plugingSrcNames << "BasicImageProcessing" << "Display" << "Data Manager" << "Image Navigator" << "LiverSegmentation" <<
+    "Measurement" << "PointSet Interaction" << "RegionGrowing Segmentation" << "Segmentation" << "Segmentation Utilities" <<
+    "Statistics" << "Vascular Structure Segmentation" << "Volume Visualization";
 }
 
 PartInfo::PartInfo(IPresentablePart::Pointer part)
@@ -33,6 +55,13 @@ PartInfo::PartInfo(IPresentablePart::Pointer part)
 void PartInfo::Set(IPresentablePart::Pointer part)
 {
   name = part->GetName().c_str();
+
+  for (int i=0; i<plugingSrcNames.size(); i++) {
+    if (plugingSrcNames[i].compare(QString(part->GetName().c_str())) == 0) {
+      name = plugingTranslateNames[i];
+    }
+  }
+  
   title = part->GetTitle().c_str();
   contentDescription = part->GetTitleStatus().c_str();
   image = static_cast<QIcon*> (part->GetTitleImage());
