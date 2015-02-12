a08fdfc4f3a67aed8039bc4318384b14429e6acc
is.moshkov <il.moshkov@gmail.com>
2015-02-12 16:19:31 +0400

    fix crash when image for planar figure not set

--- Modules/PlanarFigure/DataManagement/mitkPlanarFigure.cpp
+++ Modules/PlanarFigure/DataManagement/mitkPlanarFigure.cpp
@@ -78,7 +78,8 @@ mitk::PlanarFigure::PlanarFigure()
   m_PolyLineUpToDate(false),
   m_HelperLinesUpToDate(false),
   m_FeaturesUpToDate(false),
-  m_FeaturesMTime( 0 )
+  m_FeaturesMTime( 0 ),
+  m_ImageNode(0)
 {
   m_HelperPolyLinesToBePainted = BoolContainerType::New();
 
@@ -114,7 +115,8 @@ mitk::PlanarFigure::PlanarFigure(const Self& other)
     m_FeaturesUpToDate(other.m_FeaturesUpToDate),
     m_Features(other.m_Features),
     m_FeaturesMTime(other.m_FeaturesMTime),
-    m_DisplaySize(other.m_DisplaySize)
+    m_DisplaySize(other.m_DisplaySize),
+    m_ImageNode(other.m_ImageNode)
 {
 }
 
