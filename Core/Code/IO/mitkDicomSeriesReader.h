2766058eb49cc4ff42aaf677660b20f2345ad4d8
is.moshkov <il.moshkov@gmail.com>
2015-03-26 15:43:18 +0400

    fixing bug with non support  integer DICOM AUT-150

--- Core/Code/IO/mitkDicomSeriesReader.h
+++ Core/Code/IO/mitkDicomSeriesReader.h
@@ -552,7 +552,10 @@ public:
                               UpdateCallBackMethod callback = 0,
                               itk::SmartPointer<Image> preLoadedImageBlock = 0);
 
-  static void LoadSeries(const DicomSeriesReader::StringContainer& filenames, itk::SmartPointer<Image> image,  void* command); 
+  static void LoadSeries(const DicomSeriesReader::StringContainer& filenames, itk::SmartPointer<Image> image, itk::ImageIOBase::IOComponentType comptype,  void* command); 
+
+  template <typename PixelType>
+  static void LoadSeriesTemplate(const DicomSeriesReader::StringContainer& filenames, itk::SmartPointer<Image> image, void* command);
 
 protected:
 
