134fa9829419be86baf14ca366d2bbed471aa5fe
mpkh <mpkh@users.noreply.github.com>
2015-05-13 10:22:17 +0400

    Enable dicom plugin for MITK

--- Plugins/PluginList.cmake
+++ Plugins/PluginList.cmake
@@ -23,7 +23,7 @@ set(MITK_EXT_PLUGINS
   org.mitk.gui.qt.datamanagerlight:OFF
   org.mitk.gui.qt.properties:ON
   org.mitk.gui.qt.basicimageprocessing:OFF
-  org.mitk.gui.qt.dicom:OFF
+  org.mitk.gui.qt.dicom:ON
   org.mitk.gui.qt.diffusionimaging:OFF
   org.mitk.gui.qt.dtiatlasapp:OFF
   org.mitk.gui.qt.igtexamples:OFF
