cc39678ae978002edecafe635378430e8e889648
Vladimir <jerimaer@rambler.ru>
2015-06-17 12:41:46 +0400

    Fixed bug to work Undo\Redo algorithm

--- Plugins/org.mitk.gui.qt.ext/src/QmitkExtWorkbenchWindowAdvisor.cpp
+++ Plugins/org.mitk.gui.qt.ext/src/QmitkExtWorkbenchWindowAdvisor.cpp
@@ -439,6 +439,14 @@ dropTargetListener(new QmitkDefaultDropTargetListener)
 
 QmitkExtWorkbenchWindowAdvisor::~QmitkExtWorkbenchWindowAdvisor()
 {
+    productName = QCoreApplication::applicationName();
+    viewExcludeList.push_back("org.mitk.views.viewnavigatorview");
+
+    berry::IPreferencesService* prefService = berry::Platform::GetPreferencesService();
+    berry::IPreferences::Pointer segmentationPreferencesNode = prefService->GetSystemPreferences()->Node("/org.mitk.views.segmentation");
+
+    int value = segmentationPreferencesNode->GetInt("queue size", mitk::DEF_DEQUE_SIZE);
+    mitk::LimitedLinearUndo::setDequeSize(value);
 }
 
 berry::ActionBarAdvisor::Pointer QmitkExtWorkbenchWindowAdvisor::CreateActionBarAdvisor(
