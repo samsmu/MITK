87cfb0916e0e551e85936d845eaeb03503a0021c
Mikhail Pukhlikov <cynede@gentoo.org>
2017-10-04 15:43:29 +0400

    DicomSeriesReader::GetSeries: DICOM tags check

--- Modules/Core/src/IO/mitkDicomSeriesReader.cpp
+++ Modules/Core/src/IO/mitkDicomSeriesReader.cpp
@@ -796,6 +796,15 @@ namespace mitk
       if (std::string(fileIter->first) == std::string("DICOMDIR"))
         continue;
 
+      gdcm::Scanner::TagToValue& tagMap = const_cast<gdcm::Scanner::TagToValue&>(fileIter->second);
+      const char* seriesInstanceUid = tagMap[tagSeriesInstanceUID];
+      const char* imagePosition = tagMap[tagImagePositionPatient];
+      const char* imageOrientation = tagMap[tagImageOrientation];
+
+      if (!seriesInstanceUid || !imagePosition || !imageOrientation) {
+        continue;
+      }
+
       /* sort out multi-frame
       if ( scanner.GetValue( fileIter->first , tagNumberOfFrames ) )
       {
