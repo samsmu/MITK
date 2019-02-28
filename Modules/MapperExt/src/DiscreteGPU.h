c1063491d3e5ffc4e6f87d4631fd5a53f2af7812
Ilia F <Ilia F>
2019-02-28 16:25:46 +0400

    Refactoring AUT-4067

--- Modules/MapperExt/src/DiscreteGPU.h
+++ Modules/MapperExt/src/DiscreteGPU.h
@@ -1,10 +1,7 @@
 #if defined(_WIN32)
 extern "C" {
-  _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
-}
+  __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 
-extern "C"
-{
   __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
 #endif // WIN32
