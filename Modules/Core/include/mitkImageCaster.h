517054bb9d047b32e6b97cb31af5a8bc248604a7
Mozg90 <sapozhnikovanton@gmail.com>
2021-03-17 19:50:22 +0400

    *caster

--- Modules/Core/include/mitkImageCaster.h
+++ Modules/Core/include/mitkImageCaster.h
@@ -18,19 +18,17 @@ See LICENSE.txt or http://www.mitk.org for details.
 #define MITKIMAGECASTER_H
 
 #include <itkCastImageFilter.h>
-#include <itkExtractImageFilter.h>
 #include <itkImage.h>
-#include <itkVectorIndexSelectionCastImageFilter.h>
-
+#include <mitkImageCast.h>
+#include <mitkSurface.h>
 #include <vtkRenderWindow.h>
 
-#include <mitkImageCast.h>
-#include <mitkPPSeqForEach.h>
-#include <mitkSurface.h>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/tuple/rem.hpp>
 
-#define DeclareMitkImageCasterMethods(r, data, type) \
-  static void CastToItkImage(const mitk::Image*, itk::SmartPointer<itk::Image<MITK_PP_TUPLE_REM(2)type> >&); \
-  static void CastToMitkImage(const itk::Image<MITK_PP_TUPLE_REM(2)type>*, itk::SmartPointer<mitk::Image>&);
+#define DeclareMitkImageCasterMethods(r, data, type)                                                                   \
+  static void CastToItkImage(const mitk::Image *, itk::SmartPointer<itk::Image<BOOST_PP_TUPLE_REM(2) type>> &);         \
+  static void CastToMitkImage(const itk::Image<BOOST_PP_TUPLE_REM(2) type> *, itk::SmartPointer<mitk::Image> &);
 
 namespace mitk
 {
@@ -42,51 +40,27 @@ namespace mitk
   class MITKCORE_EXPORT ImageCaster
   {
   public:
+    BOOST_PP_SEQ_FOR_EACH(DeclareMitkImageCasterMethods, _, MITK_ACCESSBYITK_TYPES_DIMN_SEQ(2))
+    BOOST_PP_SEQ_FOR_EACH(DeclareMitkImageCasterMethods, _, MITK_ACCESSBYITK_TYPES_DIMN_SEQ(3))
 
-    MITK_PP_SEQ_FOR_EACH(DeclareMitkImageCasterMethods, _, MITK_ACCESSBYITK_TYPES_DIMN_SEQ(2))
-    MITK_PP_SEQ_FOR_EACH(DeclareMitkImageCasterMethods, _, MITK_ACCESSBYITK_TYPES_DIMN_SEQ(3))
-
-    static void CastBaseData(mitk::BaseData* const, itk::SmartPointer<mitk::Image>&);
-
+    static void CastBaseData(mitk::BaseData *const, itk::SmartPointer<mitk::Image> &);
   };
 
   class MITKCORE_EXPORT Caster
   {
   public:
-    static void Cast(BaseData* dat, Surface* surface);
+    static void Cast(BaseData *dat, Surface *surface);
   };
 
   class MITKCORE_EXPORT RendererAccess
   {
   public:
-    static void Set3DRenderer(vtkRenderer* renderer);
-    static vtkRenderer* Get3DRenderer();
+    static void Set3DRenderer(vtkRenderer *renderer);
+    static vtkRenderer *Get3DRenderer();
+
   protected:
-    static vtkRenderer* m_3DRenderer;
+    static vtkRenderer *m_3DRenderer;
   };
-
-  template<typename TPixel, unsigned int VImageDimension>
-  void extractComponentFromVectorByItk(itk::VectorImage<TPixel, VImageDimension>* itkVectorImage, mitk::Image* mitkImage, int component)
-  {
-    typedef itk::VectorImage<TPixel, VImageDimension> InputImageType;
-    typedef itk::Image<TPixel, VImageDimension> OutputImageType;
-    typedef itk::VectorIndexSelectionCastImageFilter<InputImageType, OutputImageType> ComponentFilterType;
-
-    typename ComponentFilterType::Pointer extractor = ComponentFilterType::New();
-
-    extractor->SetInput(itkVectorImage);
-    extractor->SetIndex(component);
-    extractor->Update();
-
-    mitkImage->InitializeByItk<OutputImageType>(extractor->GetOutput());
-    mitkImage->SetVolume(extractor->GetOutput()->GetBufferPointer());
-  }
-
-  template<typename TPixel, unsigned int VImageDimension = 3U>
-  void paste3Dto4DByItk(itk::Image<TPixel, VImageDimension>* itkImage3d, mitk::Image* image4d, unsigned int t)
-  {
-    image4d->SetVolume(itkImage3d->GetBufferPointer(), t);
-  }
-}
+} // namespace mitk
 
 #endif // MITKIMAGECASTER_H
